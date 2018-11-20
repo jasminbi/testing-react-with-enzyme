688b1868c67e578bee9f1ae58551fb1e3fc76bb6
Jasmin Bissig <jasmin.bissig@gmail.com>
2018-11-20 16:48:14 +0100

    Setting up create-react-app and writing frist test

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme';
+import { expect } from 'chai';
+import App from './App';
+
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() });
+
+describe('App component testing', function() {
+  it('renders welcome message', function() {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className='App-title'>Welcome to React</h1>;
+    expect(wrapper.contains(welcome)).to.equal(true);
+  });
+});
+
+
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
